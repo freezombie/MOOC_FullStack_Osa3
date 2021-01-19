cc816acf0f739feccaaa10c947684c8b05c5a2c3
FreeZombie <antti.pikkuaho@outlook.com>
2021-01-19 12:01:26 +0200

    Osa 3 vaihe A valmis.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json())
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+app.use(morgan('tiny', {
+    skip: (req, res) => {return req.method === 'POST'}
+}));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+    skip: (req, res) => { return req.method !== 'POST' }
+}));
+
+const MAX_ID = 99999;
+const MIN_ID = 10000;
 
 let persons = [
     {
@@ -50,10 +63,16 @@ app.delete('/api/persons/:id', ( request, response) => {
 })
 
 const generateId = () => {
-    const maxId =  persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0
-    return maxId + 1
+    let returnedId = 0;
+    let possiblePerson = null;
+    do {
+        const randomId = Math.floor(Math.random() * (MAX_ID - MIN_ID + 1) + MIN_ID);
+        possiblePerson = persons.find( ({ id }) => id === randomId );
+        if (!possiblePerson) {
+            returnedId = randomId;
+        }
+    } while (possiblePerson);
+    return returnedId;
 }
 
 app.post('/api/persons', (request, response) => {
@@ -67,6 +86,10 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'number missing'
         })
+    } else if (persons.find( ({name}) => name === body.name)) {
+        return response.status(409).json({
+            error: 'Name already exists'
+        })
     }
 
     const person = {
@@ -77,6 +100,12 @@ app.post('/api/persons', (request, response) => {
     persons = persons.concat(person);
     response.json(person);
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const port = 3001;
 app.listen(port);
